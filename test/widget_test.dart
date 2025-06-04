5a901d03282e75de4f57c649b59ff6cf990431b9
Kareena <angewills387@gmail.com>
2025-06-04 20:17:10 +0545

    feat:add multiples table in multiples page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:kareerbuddy/youtubeui.dart';
+import 'package:kareerbuddy/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
