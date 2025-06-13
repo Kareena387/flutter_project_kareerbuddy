03a457cb76e1255bccb59c95f25751bcc3bd1f83
Kareena <angewills387@gmail.com>
2025-06-13 23:59:57 +0545

    feat: Add auth flow with post api

--- lib/constant/pages.dart
+++ lib/constant/pages.dart
@@ -1,20 +1,31 @@
+import 'package:kareerbuddy/controllers/auth/login_controller.dart';
 import 'package:kareerbuddy/controllers/home_page_controller.dart';
 import 'package:kareerbuddy/controllers/recipe_controller.dart';
+import 'package:kareerbuddy/views/auth/login_page.dart';
 import 'package:kareerbuddy/views/home_page.dart';
 import 'package:kareerbuddy/views/recipe_details_page.dart';
 import 'package:get/get.dart';
 
 final List<GetPage> getPages = <GetPage>[
   GetPage(
-      name: HomePage.routeName,
-      page: () => HomePage(),
-      binding: BindingsBuilder(() {
-        Get.lazyPut(() => HomePageController());
-      })),
+    name: HomePage.routeName,
+    page: () => HomePage(),
+    binding: BindingsBuilder(() {
+      Get.lazyPut(() => HomePageController());
+    }),
+  ),
   GetPage(
-      name: RecipeDetailsPage.routeName,
-      page: () => RecipeDetailsPage(),
-      binding: BindingsBuilder(() {
-        Get.lazyPut(() => RecipeController());
-      })),
+    name: LoginPage.routeName,
+    page: () => LoginPage(),
+    binding: BindingsBuilder(() {
+      Get.lazyPut(() => LoginController());
+    }),
+  ),
+  GetPage(
+    name: RecipeDetailsPage.routeName,
+    page: () => RecipeDetailsPage(),
+    binding: BindingsBuilder(() {
+      Get.lazyPut(() => RecipeController());
+    }),
+  ),
 ];
