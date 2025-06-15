78c69c63a994afb8b6abc47965558f581c0b0005
Kareena <angewills387@gmail.com>
2025-06-15 17:37:49 +0545

    feat: add CoreStorageService using GetStorage for token management

--- lib/constant/api.dart
+++ lib/constant/api.dart
@@ -1,5 +1,5 @@
 class Api {
   static const String baseUrl = "https://dummyjson.com";
   static const String loginUrl = "$baseUrl/auth/login";
-  static const String profileUrl = "$baseUrl/auth/me";
+  static const String profileUrl = "$baseUrl/auth/me";   // Profile fetch endpoint
 }
--- lib/core_storage/core_storage_service.dart
+++ lib/core_storage/core_storage_service.dart
@@ -2,9 +2,9 @@ import 'package:get_storage/get_storage.dart';
 
 class CoreStorageService {
   static final GetStorage_box = GetStorage();
-   static void saveToken(String token) => _box.write('token', token);
-  static String? getToken() => _box.read('token');
-  static void clearToken() => _box.remove('token');
-  static bool isLoggedIn() => _box.hasData('token');
+  static void saveToken(String token) => _box.write('token', token);  //save token
+  static String? getToken() => _box.read('token');   //read token
+  static String? clearToken() => _box.remove('token');  //clear token to  logout
+  static String? isLoggedINToken() => _box.hasData('token'); //check login status
 
 }
