0d02892cb228ab5bd2b03695349349645e54ea96
Kareena <angewills387@gmail.com>
2025-06-15 17:42:27 +0545

    feat: add LoginResponse model for parsing login token

--- lib/model/login_response.dart
+++ lib/model/login_response.dart
@@ -1,2 +1,10 @@
-class LoginResponse ({required this.token});
-fa
+class LoginResponse {
+  final String token;
+
+  LoginResponse({required this.token});
+
+  //converts JSOn to Dart object
+  factory LoginResponse.formJson(Map<String, dynamic> json){
+    return LoginResponse(token: json['token']);
+  }
+}
