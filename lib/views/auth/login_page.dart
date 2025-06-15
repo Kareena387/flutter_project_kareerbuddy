b172519cedb148d7668167361adaf800eb43dedc
Kareena <angewills387@gmail.com>
2025-06-15 18:14:18 +0545

    feat:build loginpage UI with controller integration-changed plogin

--- lib/views/auth/login_page.dart
+++ lib/views/auth/login_page.dart
@@ -1,7 +1,8 @@
-import 'package:kareerbuddy/controllers/auth/login_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kareerbuddy/controllers/auth/login_controller.dart';
 
+// LoginPage widget
 class LoginPage extends StatelessWidget {
   static String routeName = "/login-page";
   final c = LoginController();
@@ -12,61 +13,29 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Login Page")),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: c.username,
-
-                decoration: InputDecoration(
-                  hintText: "User name/Email",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                ),
-
-              ),
-
-              SizedBox(height: 20),
-              TextField(
-                controller: c.password,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Password",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                ),
-              ),
-              SizedBox(height: 120),
-              InkWell(
-                onTap: (){
-                  c.login();
-                },
-                child: Container(
-                  height: 48,
-                  width: Get.width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.blue,
-                  ),
-                  child: Center(child: Text("Login")),
-                ),
-              ),
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Username Field
+            TextField(
+              controller: c.username,
+              decoration: InputDecoration(hintText: "Username/Email"),
+            ),
+            SizedBox(height: 20),
+            // Password Field
+            TextField(
+              controller: c.password,
+              obscureText: true,
+              decoration: InputDecoration(hintText: "Password"),
+            ),
+            SizedBox(height: 40),
+            // Login Button
+            ElevatedButton(
+              onPressed: c.login,
+              child: Text("Login"),
+            ),
+          ],
         ),
       ),
     );
