2f6f4c2330b5533dd06f2ed43068db7f6a3a1d93
Kareena <angewills387@gmail.com>
2025-06-15 18:01:53 +0545

    feat: implement AuthService for login and profile API calls

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:kareerbuddy/core_storage/core_storage_service.dart';
+import 'package:kareerbuddy/model/login_response.dart';
+import 'package:kareerbuddy/model/user_model.dart';
+
+class AuthService {
+  //Sends login request and saves token
+  Future<bool> login(String username, String password) async {
+    final response = await http.post(
+      Uri.parse(Api.loginUrl),
+      headers: {'Content-Type': ' application/json'},
+      body: jsonEncode({'username': username, 'password': password}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = LoginResponse.fromJson(jsonDecode(response.body));
+      CoreStorageService.saveToken(data.token); // Save token on success
+      return true;
+    }
+    return false; // Login failed
+  }
+
+  // Gets user profile using stored token
+  Future<UserModel?> getProfile() async {
+    final token = CoreStorageService.getToken();
+    if (token == null) return null;
+
+    final response = await http.get(
+      Uri.parse(Api.profileUrl),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode == 200) {
+      return UserModel.fromJson(jsonDecode(response.body));
+    }
+    return null;
+  }
+}
